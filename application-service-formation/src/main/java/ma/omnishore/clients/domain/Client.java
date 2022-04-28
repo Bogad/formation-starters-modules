f4d5d61fd898d53b36cc622d9e4ae48cd3a71732
Amine Bouaggad <Abouaggad@omnidata.ma>
2022-04-28 12:45:50 +0000

    step02

---
+++ application-service-formation/src/main/java/ma/omnishore/clients/domain/Client.java
@@ -0,0 +1,84 @@
+package ma.omnishore.clients.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ * A Client.
+ */
+@Entity
+@Table(name = "client")
+public class Client implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name = "last_name")
+	private String lastName;
+	@Column(name = "first_name")
+	private String firstName;
+	@Column(name = "email")
+	private String email;
+	@Column(name = "id_number")
+	private String idNumber;
+	@ManyToOne
+	private Country country;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Country getCountry() {
+		return country;
+	}
+
+	public void setCountry(Country country) {
+		this.country = country;
+	}
+
+	public String getIdNumber() {
+		return idNumber;
+	}
+
+	public void setIdNumber(String idNumber) {
+		this.idNumber = idNumber;
+	}
+
+}
