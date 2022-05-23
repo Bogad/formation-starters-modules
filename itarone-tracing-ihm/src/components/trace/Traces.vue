c48e5f3b2277412d1aee87e2e021e7247a5c7438
Amine Bouaggad <Abouaggad@omnidata.ma>
2022-05-23 13:21:30 +0100

    step19

--- itarone-tracing-ihm/src/components/trace/Traces.vue
+++ itarone-tracing-ihm/src/components/trace/Traces.vue
@@ -1,6 +1,27 @@
 <template>
+<div>
+  <b-container>
+    <br/>
+      <b-row class="justify-content-md-end">
+                  <b-col col lg="1">
+                    <b-button v-if="$keycloak.authenticated" v-on:click="logout()" id="logout">
+                        <font-awesome-icon class="mr-2" icon="sign-out-alt" />
+                        <span>Déconnexion</span></b-button>
+                  </b-col>
+                 
+      </b-row>
+              </b-container>
   <router-view></router-view>
+</div>
 </template>
 
 <script>
+export default {
+  methods: {
+    logout() {
+      this.$keycloak.logoutFn();
+      this.$keycloak.push("/");
+    }
+  }
+};
 </script>
